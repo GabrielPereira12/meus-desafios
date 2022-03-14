dbf9add38915c046209cad0c2293988a43396f09
Gabriel <96059001+GabrielPereira12@users.noreply.github.com>
2022-03-14 15:51:37 +0000

    0.2

--- jogo de palavras/script.js
+++ jogo de palavras/script.js
@@ -1,4 +1,8 @@
 function start(){
     var dv = document.getElementById("esconde");
+    var block = document.getElementById("blocos");
     dv.hidden = false;
+
+    block.split("");
+    window.alert(`${block.split('')}`)
 }
