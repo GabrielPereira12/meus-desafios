e2902a829b0fe64f042cc263c15c47e2f9b22a8a
Gabriel <96059001+GabrielPereira12@users.noreply.github.com>
2022-03-21 13:15:21 +0000

    atualização no javascript (incompleto)

--- jogo de palavras/script.js
+++ jogo de palavras/script.js
@@ -1,29 +1,102 @@
-let pergunta = {
+const corretaSom = new Audio('sons/correct.mp3');
+const erradaSom = new Audio('sons/wrong.mp3')
+corretaSom.volume -= 0.8;
+erradaSom.volume -= 0.8;
+
+
+let perguntas = [
+    {
     titulo: "Gato",
-    alternativas: ["Gat", "Cat", "Gate", "Dog"],
+    alternativas: ["Rat", "Cat", "Gate", "Dog"],
     correta: 1
-};
+    },
+    
+    {
+    titulo: "Alligator",
+    alternativas: ["Motor", "Jaré", "Alicate", "Jacaré"],
+    correta: 3
+    },
+    
+    {
+    titulo: "Tombstone",
+    alternativas: ["Lápide", "Cascalho", "Pedra", "Cova"],
+    correta: 0
+    }
+]
 
-function start() {
+let app = {
+    start: function() {
+        this.atualPos = 0;
+        this.totalPontos = 0;
 
-    let alts = document.querySelectorAll('.alternativa');
-    alts.forEach(function(element, index) {
-        element.addEventListener('click', function(){
-            console.log("Checa resposta")
-        });
-    });
-    mostraquestao(pergunta);
+        let alts = document.querySelectorAll('.alternativa');
+        alts.forEach((element, index)=>{
+            element.addEventListener('click', ()=>{
+                this.checaResposta(index);
+                this.mostraResposta(index);
+            })
+        })
+        this.atualizaPontos();
+        this.mostraquestao(perguntas[this.atualPos]);
+    },
+
+    mostraquestao: function(q){
+        this.qatual = q;
+        // mostrando o titulo
+        let titleDiv = document.getElementById('titulo');
+        titleDiv.textContent = q.titulo;
+        // mostrando as alternativas
+        let alts = document.querySelectorAll('.alternativa');
+        alts.forEach(function(element, index) {
+            element.textContent = q.alternativas[index];
+        })
+    },
+
+    proximaPerg: function(){
+        this.atualPos++;
+        if(this.atualPos == perguntas.length){
+            this.atualPos = 0;
+        }
+    },
+
+    checaResposta: function(user){
+        if(this.qatual.correta == user){
+            console.log("Correta");
+            //corretaSom.play();
+            this.totalPontos++;
+        }
+        else{
+            console.log("Errada!");
+            //erradaSom.play();
+        }
+
+        this.atualizaPontos();
+        this.proximaPerg();
+        this.mostraquestao(perguntas[this.atualPos]);
+    },
+
+    atualizaPontos: function(){
+        let scoreDiv = document.getElementById('pontos');
+        scoreDiv.textContent = `Sua pontuação é: ${this.totalPontos}`;
+    },
+
+    mostraResposta: function(correto){
+        let resultDiv = document.getElementById('result');
+        let result = '';
+        // formatar como a mensagem será exibida
+        if(this.qatual.correta == correto){
+            result = 'Resposta Correta!';
+        }
+        else{
+            // obtendo a questão atual
+            let pergunta = perguntas[this.atualPos];
+            // obtenha o indice da resposta correta da questão atual
+            let cIndice = pergunta.correta;
+            // obtenha o texto da resposta correta da questão atual
+            let cTexto = pergunta.alternativas[cIndice];
+            result = `Incorreto! Resposta Correta: ${cTexto}`;
+        }
+        resultDiv.textContent = result;
+    }
 }
-
-function mostraquestao(q){
-    // mostrando o titulo
-    let titleDiv = document.getElementById('titulo');
-    titleDiv.textContent = q.titulo;
-    // mostrando as alternativas
-    let alts = document.querySelectorAll('.alternativa');
-    alts.forEach(function(element, index) {
-        element.textContent = q.alternativas[index];
-    });
-}
-
-start();
+app.start(); 
