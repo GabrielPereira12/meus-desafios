72e700999612682532188bf0d860e4995bbd2c56
Gabriel Pereira <gabriel.tdp12@gmail.com>
2022-05-23 17:55:33 +0100

    dia 21

---
+++ login-react/react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import LoginPage from './pages/loginPage/index'
+import HomePage from './pages/homePage/index'
+
+function App() {
+
+  return (
+    <div className="App">
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default App
